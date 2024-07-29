2925209c8ccd83bf309b3f634788289f2b56791a
vl <gztrk@engineer.com>
2024-07-30 01:21:36 +0400

    add interface + replacement struct

--- xyz-misc/price-calculator/main.go
+++ xyz-misc/price-calculator/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/iam-vl/go-cookbook/xyz-misc/price-calculator/filemanager"
+	"github.com/iam-vl/go-cookbook/xyz-misc/price-calculator/cmdmanager"
 	"github.com/iam-vl/go-cookbook/xyz-misc/price-calculator/prices"
 )
 
@@ -11,8 +9,9 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		cmdm := cmdmanager.New()
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
--- xyz-misc/price-calculator/prices/prices.go
+++ xyz-misc/price-calculator/prices/prices.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 
 	"github.com/iam-vl/go-cookbook/xyz-misc/price-calculator/conversion"
-	"github.com/iam-vl/go-cookbook/xyz-misc/price-calculator/filemanager"
+	"github.com/iam-vl/go-cookbook/xyz-misc/price-calculator/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
 	// add struct tags
-	IOManager         filemanager.FileManager `json:"-"` // ignore in the outpur
-	TaxRate           float64                 `json:"tax_rate"`
-	InputPrices       []float64               `json:"input_prices"`
-	TaxIncludedPrices map[string]string       `json:"tax_included_prices"`
+	IOManager         iomanager.IOManager `json:"-"` // ignore in the output
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -42,9 +42,9 @@ func (job *TaxIncludedPriceJob) Process() {
 	job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		IOManager:   fm,
+		IOManager:   iom,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
