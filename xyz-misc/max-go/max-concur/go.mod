fb6e360e0e1d73d9606b391f21a355b983941c64
vl <gztrk@engineer.com>
2024-08-06 18:04:38 +0300

    add some max info

---
+++ xyz-misc/max-go/max-concur/go.mod
@@ -0,0 +1,3 @@
+module github.com/iam-vl/go-cookbook/tree/max-go/xyz-misc/max-go/max-concur
+
+go 1.22.4
--- xyz-misc/max-go/max-concur/main.go
+++ xyz-misc/max-go/max-concur/main.go
@@ -6,16 +6,25 @@ import (
 )
 
 func main() {
-	go greet("Nice to meet you!")
-	go greet("How are you?")
-	go slowGreet("How ... are ... you ...?")
-	go greet("I hope you're liking the course!")
+	doneCh := make(chan bool)
+	go greet("Nice to meet you!", doneCh)
+	go greet("How are you?", doneCh)
+	go slowGreet("How ... are ... you ...?", doneCh)
+	go greet("I hope you're liking the course!", doneCh)
+
+	for isDone := range doneCh {
+		fmt.Println(isDone)
+	}
+
 }
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
+	close(doneChan)
 }
